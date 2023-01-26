50569897b5bbdde01a34460bf256405e3d9dd6a5
ProFIT1337 <santiago.farias@yandex.ru>
2023-01-26 14:40:09 +0700

    finish game

---
+++ script.js
@@ -0,0 +1,43 @@
+let leftButton = document.querySelector('.leftButton');
+let rightButton = document.querySelector('.rightButton');
+let buttons = [leftButton, rightButton];
+let activeButton;
+let startTime;
+let bestRez = 150000;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function activateButton(button) {
+  startTime = new Date();
+  button.classList.add('active');
+  activeButton = button;
+}
+
+function finishGame() {
+  let rez = new Date() - startTime;
+  if (rez < bestRez) {
+    bestRez = rez;
+    document.querySelector('.res__value').innerText = rez + 'ms';
+  }
+  activeButton.classList.remove('active');
+  activeButton = undefined;
+  setTimeout(() => {
+    activateButton(buttons[getRandomInt(1)]);
+  }, getRandomInt(10000));
+}
+
+function clickHandler(e) {
+  if (e.target == activeButton && activeButton.classList.contains('active')) {
+    finishGame();
+  }
+}
+
+function start() {
+  leftButton.onclick = clickHandler;
+  rightButton.onclick = clickHandler;
+  activateButton(leftButton);
+}
+
+start();
